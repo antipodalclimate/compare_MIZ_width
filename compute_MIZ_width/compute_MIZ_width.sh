20b2aeaa205ec4de6c6eeb664baea4f54dd965fd
christopher_horvat@brown.edu <chorvat@node1902.oscar.ccv.brown.edu>
2024-10-22 13:53:51 -0400

    parallelization

---
+++ compute_MIZ_width/compute_MIZ_width.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -t 12:00:00
+#SBATCH -n 1
+#SBATCH --mem=0
+#SBATCH --account=epscor-condo
+#SBATCH --mail-user=bndnchrs@gmail.com 
+#SBATCH --mail-type=ALL
+#SBATCH -J=bybeam
+# #SBATCH --constraint=skylake
+# #SBATCH --exclusive
+
+module load matlab
+matlab-threaded -r drive_MIZ_width
+
+
+
