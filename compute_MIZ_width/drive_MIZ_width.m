8585b8e11993ee6ecd8fbdfd19bca9652e185727
chhorvat <christopher_horvat@brown.edu>
2024-10-22 12:37:56 -0400

    updated for more generality

--- compute_MIZ_width/drive_MIZ_width.m
+++ compute_MIZ_width/drive_MIZ_width.m
@@ -1,9 +1,18 @@
 clear
 
-OPTS.data_folder = '/Users/chorvat/Brown Dropbox/Christopher Horvat/Research Projects/Active/Data/ICESat-2/';
+% Location of the overall data
+OPTS.data_folder = '/Users/chorvat/Brown Dropbox/Christopher Horvat/Research Projects/Active/Data/ICESat-2/PM-SIC-width/Brouwer-Tracks/';
+
+% Location of this code. 
 OPTS.code_folder = '/Users/chorvat/Code/compare_MIZ_width/'; 
+
+% Location of utility files, output directory, and tracks. 
 OPTS.utils_folder = [OPTS.code_folder 'Utilities']; % Location of util files
-OPTS.output_folder = [OPTS.data_folder 'PM-SIC-width/Brouwer-Tracks/Compiled_Stats/']; 
+OPTS.output_folder = [OPTS.data_folder 'Compiled_Stats/']; 
+OPTS.track_folder = [OPTS.data_folder 'Raw_Tracks_v6/'];
+
+% Updatable name of output. 
+OPTS.output_str = [OPTS.output_folder 'AT_stats_SH_remake_v6'];  
 
 OPTS.do_weak = 0; % Don't use the weak beams
 OPTS.AT_resolution = 6250; % The resolution of the along-track data
@@ -11,12 +20,11 @@ OPTS.beamnames = {'/gt1r','/gt1l','/gt2r','/gt2l','/gt3r','/gt3l'};
 
 %% Necessary components for the location of IS2 data
 
-OPTS.folder = [OPTS.data_folder 'PM-SIC-width/Brouwer-Tracks/Raw_Tracks_v6/'];
-OPTS.filenames = cat(1,dir([OPTS.folder '*.nc']),dir([OPTS.folder '*.h5']));
+
+OPTS.filenames = cat(1,dir([OPTS.track_folder '*.nc']),dir([OPTS.track_folder '*.h5']));
 OPTS.nfiles = length(OPTS.filenames);
 
 % Where do we put the output
-OPTS.output_str = [OPTS.output_folder 'AT_stats_SH_remake_v5'];  
 
 addpath(OPTS.utils_folder); 
 
@@ -30,13 +38,3 @@ create_AT_stat_file(OPTS);
 OPTS.load_string = OPTS.output_str; 
 
 reference_stats_to_MIZ(OPTS);
-
-%% Second Figure
-
-addpath('Figure-2-Brouwer/')
-addpath('Utilities/Plotting/')
-
-make_figure_brouwer_comp(OPTS)
-
-%
-% make_figure_brouwer_split;
