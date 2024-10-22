67ebab65701b68e88a9d097628e82abf79bdf384
chhorvat <christopher_horvat@brown.edu>
2024-10-22 13:56:49 -0400

    updates

--- compute_MIZ_width/drive_MIZ_width.m
+++ compute_MIZ_width/drive_MIZ_width.m
@@ -1,25 +1,26 @@
 clear
 
 % Location of the overall data
-OPTS.data_folder = '/Users/chorvat/Brown Dropbox/Christopher Horvat/Research Projects/Active/Data/ICESat-2/PM-SIC-width/Brouwer-Tracks/';
+OPTS.track_folder = '/Users/chorvat/Brown Dropbox/Christopher Horvat/Research Projects/Active/Data/ICESat-2/PM-SIC-width/Brouwer-Tracks/Raw_Tracks_v6/';
 
-% Location of this code. 
-OPTS.code_folder = '/Users/chorvat/Code/compare_MIZ_width/'; 
-
-% Location of utility files, output directory, and tracks. 
-OPTS.utils_folder = [OPTS.code_folder 'Utilities']; % Location of util files
-OPTS.output_folder = [OPTS.data_folder 'Compiled_Stats/']; 
-OPTS.track_folder = [OPTS.data_folder 'Raw_Tracks_v6/'];
+% Updatable name of input
+OPTS.output_folder = '/Users/chorvat/Brown Dropbox/Christopher Horvat/Research Projects/Active/Data/ICESat-2/PM-SIC-width/Brouwer-Tracks/Compiled_Stats/';
 
 % Updatable name of output. 
-OPTS.output_str = [OPTS.output_folder 'AT_stats_SH_remake_v6'];  
+OPTS.output_str = [OPTS.output_folder 'AT_stats_SH_v6'];
+
+%%
+% Location of this code. 
+OPTS.code_folder = '~/Code/compare_MIZ_width/';
+% Location of utility files, output directory, and tracks. 
+OPTS.utils_folder = [OPTS.code_folder 'Utilities']; % Location of util files
+
 
 OPTS.do_weak = 0; % Don't use the weak beams
 OPTS.AT_resolution = 6250; % The resolution of the along-track data
 OPTS.beamnames = {'/gt1r','/gt1l','/gt2r','/gt2l','/gt3r','/gt3l'};
 
 %% Necessary components for the location of IS2 data
-
 
 OPTS.filenames = cat(1,dir([OPTS.track_folder '*.nc']),dir([OPTS.track_folder '*.h5']));
 OPTS.nfiles = length(OPTS.filenames);
