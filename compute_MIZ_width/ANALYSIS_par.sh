8075b0dc3c4d98625163b8f2a3b207eb64bffff4
christopher_horvat@brown.edu <chorvat@node1902.oscar.ccv.brown.edu>
2024-11-20 18:45:35 -0500

    updated OSCAR branch

--- compute_MIZ_width/ANALYSIS_par.sh
+++ compute_MIZ_width/ANALYSIS_par.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#SBATCH -t 4:00:00
-#SBATCH -n 8
+#SBATCH -t 8:00:00
+#SBATCH -n 16
+#SBATCH -N 1
 #SBATCH --mem=0
 #SBATCH --account=epscor-condo
 #SBATCH --mail-user=bndnchrs@gmail.com 
@@ -10,7 +11,6 @@
 # #SBATCH --constraint=skylake
 # #SBATCH --exclusive
 
-matlab-threaded -r drive_AT_stats_par
-
-
+module load matlab
+matlab-threaded -r drive_MIZ_width
 
